8c37fe53739a61191c8bff21d724d40750961075
pedro <151687035+pedrokunrath@users.noreply.github.com>
2024-08-12 20:56:01 -0300

    .

--- aula_prova/script.js
+++ aula_prova/script.js
@@ -1,11 +1,18 @@
+
 function Mensagem() {
-    
-    const nome = document.getElementById('Nome').value;
-    const email = document.getElementById('Email').value;
-    const mensagem = Muito obrigado(a), senhor(a) ${nome}. Acabamos de enviar um e-mail para: ${email};
- 
+  
+    const nome = document.getElementById('nome').value;
+    const email = document.getElementById('email').value;
+
+    const mensagem = `Muito obrigado(a), senhor(a) ${nome}. Acabamos de enviar um e-mail para: ${email}`;
     document.getElementById('mensagem_confirmacao').innerText = mensagem;
 }
-function onclick(Event){
-    const clickedEnviar = Event.target;
+
+function onclick(event) {
+    const clickedEnviar = event.target;
+
+    
+    if (clickedEnviar.tagName === 'BUTTON') {
+        Mensagem();
+    }
 }
